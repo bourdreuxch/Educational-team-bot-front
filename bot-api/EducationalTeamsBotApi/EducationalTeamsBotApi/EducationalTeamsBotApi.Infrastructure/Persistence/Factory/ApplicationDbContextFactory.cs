2b24085052da5a33afcd5b412c9262fa68a24cd9
Benjamin Sorriaux <benjamin.sorriaux@diiage.org>
2022-03-06 11:09:28 +0100

    Add base for the bot API.

---
+++ bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi/EducationalTeamsBotApi.Infrastructure/Persistence/Factory/ApplicationDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ApplicationDbContextFactory.cs" company="DIIAGE">
+// Copyright (c) DIIAGE 2022. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using EducationalTeamsBotApi.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EducationalTeamsBotApi.Infrastructure.Factory
+{
+    /// <summary>
+    /// Class Factory for the <see cref="ApplicationDbContext"/>.
+    /// </summary>
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        /// <summary>
+        /// Creates a <see cref="ApplicationDbContext"/>.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
+        /// <returns>Returns a <see cref="ApplicationDbContext"/>.</returns>
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\\mssqllocaldb;Database=PocArchiCleanDb;Trusted_Connection=True;MultipleActiveResultSets=true;",
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+
+            return new ApplicationDbContext(optionsBuilder.Options, null, null);
+        }
+    }
+}
---
+++ bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi/EducationalTeamsBotApi.WebApi/Program.cs
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="DIIAGE">
+// Copyright (c) DIIAGE 2022. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using EducationalTeamsBotApi.Application;
+using EducationalTeamsBotApi.Application.Common.Interfaces;
+using EducationalTeamsBotApi.Infrastructure;
+using EducationalTeamsBotApi.WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddSingleton<ICurrentUserService, CurrentUserService>();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
