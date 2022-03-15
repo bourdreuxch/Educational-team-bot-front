fa3e0085b819f33f0134e1b6d0c48eb7d9e27e96
bourdreux charlelie <charlelie.bourdreux@diiage.org>
2022-03-15 20:24:55 +0100

    Creation of an empty page tag

--- educational-teams-bot/src/app/features/features-routing.module.ts
+++ educational-teams-bot/src/app/features/features-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
 import { QuestionsComponent } from './questions/components/questions/questions.component';
+import { TagsComponent } from './tags/components/tags/tags.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'questions',
     component: QuestionsComponent
   }
+  {
+    path: 'tags',
+    component: TagsComponent,
+  },
 ];
 
 @NgModule({
