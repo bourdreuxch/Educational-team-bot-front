019f1fdedd8dbf4948b695c7ff9c5afe3168b890
bourdreux charlelie <charlelie.bourdreux@diiage.org>
2022-03-15 20:24:55 +0100

    Creation of an empty page tag

--- educational-teams-bot/src/app/features/features-routing.module.ts
+++ educational-teams-bot/src/app/features/features-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+import { TagsComponent } from './tags/components/tags/tags.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'tags',
+    component: TagsComponent,
   },
 ];
 
---
+++ educational-teams-bot/src/app/features/tags/components/tags/tags.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss']
+})
+export class TagsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
