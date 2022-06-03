ae4cb1e3569e0bbfde8fab36f6777e53e05a1545
Nicolas Civade <mopigno@gmail.com>
2022-06-03 08:37:30 +0200

    UPD: Add controller + query | new bugs found

---
+++ bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi/EducationalTeamsBotApi.Application/Speakers/Queries/GetSpeakersQuery/GetSpeakerQuery.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetSpeakerQuery.cs" company="DIIAGE">
+// Copyright (c) DIIAGE 2022. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+
+namespace EducationalTeamsBotApi.Application.Speakers.Queries.GetSpeakersQuery
+{
+    using EducationalTeamsBotApi.Domain.Entities;
+    using MediatR;
+
+    /// <summary>
+    /// Get the list of speakers
+    /// </summary>
+    public class GetSpeakerQuery : IRequest<IEnumerable<CosmosSpeaker>>
+    {
+    }
+}
---
+++ bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi/EducationalTeamsBotApi.Application/Speakers/Queries/GetSpeakersQuery/GetSpeakerQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetSpeakerQueryHandler.cs" company="DIIAGE">
+// Copyright (c) DIIAGE 2022. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace EducationalTeamsBotApi.Application.Speakers.Queries.GetSpeakersQuery
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EducationalTeamsBotApi.Application.Common.Interfaces;
+    using EducationalTeamsBotApi.Domain.Entities;
+    using MediatR;
+
+    /// <summary>
+    /// Handler for the query that will get speakers.
+    /// </summary>
+    public class GetSpeakerQueryHandler : IRequestHandler<GetSpeakerQuery, IEnumerable<CosmosSpeaker>>
+    {
+
+        private readonly ISpeakerCosmosService speakerCosmosService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetSpeakerQueryHandler"/> class.
+        /// </summary>
+        /// <param name="speakerCosmosService">Injection.</param>
+        public GetSpeakerQueryHandler(ISpeakerCosmosService speakerCosmosService)
+        {
+            this.speakerCosmosService = speakerCosmosService;
+        }
+
+        /// <inheritdoc/>
+        public Task<IEnumerable<CosmosSpeaker>> Handle(GetSpeakerQuery request, CancellationToken cancellationToken)
+        {
+            return this.speakerCosmosService.GetCosmosSpeakers();
+        }
+    }
+}
