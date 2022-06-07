9e0816ecc41f5ee8ef741f1f725eee8a044b304c
liote geoffrey <geoffrey.liote@bwagence.fr>
2022-06-07 08:25:38 +0200

    in progress

--- educational-teams-bot/src/app/features/speakers/speakers.module.ts
+++ educational-teams-bot/src/app/features/speakers/speakers.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 import { SpeakerItemComponent } from './components/speaker-item/speaker-item.component';
+import { AutoTableComponent } from './components/auto-table/auto-table.component';
+import { AutoListComponent } from './components/auto-list/auto-list.component';
+import { AutoUpsertComponent } from './components/auto-upsert/auto-upsert.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     SpeakersComponent,
-    SpeakerItemComponent
+    SpeakerItemComponent,
+    AutoTableComponent,
+    AutoListComponent,
+    AutoUpsertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule 
   ]
 })
 export class SpeakersModule { }
